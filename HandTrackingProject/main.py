0ed5fcb9361ced89228cb4f8f1ddf8093fa80a14
parthdharmale <78959166+parthdharmale@users.noreply.github.com>
2023-06-13 10:29:22 +0530

    Add files via upload

---
+++ HandTrackingProject/main.py
@@ -0,0 +1,42 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import math
+import numpy as np
+import cvzone
+
+# Webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+# Hand Detector
+detector = HandDetector(detectionCon=0.8, maxHands=1)
+
+# Find Function
+# x is the raw distance, y is the value in cm
+x = [300, 245, 200, 170, 145, 130, 112, 103, 93, 87, 80, 75, 70, 67, 62, 59, 57]
+y = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+
+# Linear regression
+A, B = np.polyfit(x, y, 1)  # y = Ax + B
+
+# Loop
+while True:
+    success, img = cap.read()
+    hands = detector.findHands(img, draw=False)
+
+    if hands:
+        lmList = hands[0]['lmList']
+        x, y, w, h = hands[0]['bbox']
+        x1, y1 = lmList[5][:2]
+        x2, y2 = lmList[17][:2]
+
+        distance = int(math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2))
+        distanceCM = A * distance + B
+
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 3)
+        cvzone.putTextRect(img, f'{int(distanceCM)} cm', (x + 5, y - 10))
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
